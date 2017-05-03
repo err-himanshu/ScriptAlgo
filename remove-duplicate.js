38445b5002435b44798fc8681aa3d13ce45bfd9f
baldor1 <baldor1@linux-68wx.suse>
2017-05-03 15:23:30 +0530

    javascript remove duplicate from array O(n) complexity

---
+++ remove-duplicate.js
@@ -0,0 +1,27 @@
+// Problem Statement -  remove duplicates from an array
+// Example - input - [1,2,3,4,4,45,53,3,2]
+// Output -  [1,2,3,4,45,53]
+// Time Complexity - O(n)
+// Space Complexity - O(1) ; constant
+
+  function removeDuplicate(data){
+   //first check if given param is an array or not
+    if(data instanceof Array){
+      //now check for length if less than one return the same
+      if(data.length < 2)
+        return data;
+      else{
+        let result = [];
+        data.forEach(function(element){
+          //now check if result array has the element or not if yes do not push else push into this array
+            if(result.indexOf(element) === -1){
+              result.push(element);
+            }
+        });
+        return result;
+      }
+  }else {
+    return 'Not a valid array';
+  }
+}
+console.log(removeDuplicate([1,2,3,4,4,45,53,3,2]));
